fc736ab7822396612e8988c71b37c8ea03642efc
killercd <killercd@gmail.com>
2023-06-09 00:30:04 +0200

    add inc ip

--- pyscanner/ftp/ftp_scanner.py
+++ pyscanner/ftp/ftp_scanner.py
@@ -1,3 +1,4 @@
+from ftplib import FTP
 
 class FtpScanner():
     """Ftp scanner module
@@ -15,5 +16,30 @@ class FtpScanner():
         :param end_ip: End ip
         :return: returns a list of ip
         """
+
+    def _incIP(self,ip):
+        a,b,c,d = ip.split(".")
+
+        a=int(a)
+        b=int(b)
+        c=int(c)
+        d=int(d)
+
+        if d<255:
+            d=d+1
+        elif c<255:
+            c=c+1
+            d=0
+        elif b<255:
+            b=b+1
+            d=0
+            c=0
+        elif a<255:
+            a=a+1
+            d=0
+            c=0
+            b=0
+        return "{}.{}.{}.{}".format(a,b,c,d)
+
 
 
